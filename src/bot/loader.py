af9ccef40fcb1faeeb530a05f25c744a3be4b183
NoLogs99 <nologs99@gmail.com>
2026-02-27 20:44:18 +0300

    Update loader.py

--- src/bot/loader.py
+++ src/bot/loader.py
@@ -1,1 +1,15 @@
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
 
+from src.config.config import config
+from src.utils.logger import logger
+
+logger.info("Инициализация бота и диспетчера")
+logger.debug("Используется ParseMode: {}", ParseMode.HTML)
+
+bot = Bot(token=config.TELEGRAM_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+logger.success("Bot и Dispatcher успешно созданы")
