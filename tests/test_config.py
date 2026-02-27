ed7886f258161aa739c5e13f6545e217d850531f
NoLogs99 <nologs99@gmail.com>
2026-02-27 18:34:11 +0300

    Update test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,1 +1,9 @@
+from src.config.config import Config 
 
+print(Config.DEBUG)
+print(Config.TELEGRAM_TOKEN)
+print(Config.BOT_USERNAME)
+print(Config.SCRIPTS_PATH)
+print(Config.LOGS_PATH)
+print(Config.DOCS_PATH)
+print(Config.TESTS_PATH)
