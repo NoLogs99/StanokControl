f5fdf4d6c2130cd33434a5fb5d13c96f1fd3a710
NoLogs99 <nologs99@gmail.com>
2026-02-28 16:06:56 +0300

    Update __init__.py

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,1 +1,6 @@
-
+from .role import Role
+from .user import User
+from .equipment import Equipment, EquipmentType, Location
+from .maintenance import MaintenanceLog
+from .notification import Notification
+from .action_log import ActionLog
